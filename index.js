ecb674ad436f143dd5a5c7380b77cb416ba961da
aurrelkondi <aurrelkondi@hotmail.com>
2023-02-06 15:26:04 +0100

    Task1 and 2 is done

---
+++ index.js
@@ -0,0 +1,33 @@
+/* const js = "JS is amazing";
+
+let = 20 + 30 + 2;
+console.log(let);
+
+console.log(typeof null); */
+
+// 1) Values and Variables
+// 1. Declarevariablescalled'country','continent'and'population'and assign their values according to your own country (population in millions)
+// 2. Logtheirvaluestotheconsole
+
+/* 
+let coutry = "Germany";
+let continent = "Europe";
+let population = 830000;
+
+console.log(coutry);
+console.log(continent);
+console.log(population); */
+
+// 2) Data Types
+// 1. Declareavariablecalled'isIsland'andsetitsvalueaccordingtoyour country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+// 2. Logthetypesof'isIsland','population','country'and'language' to the console
+
+/*  
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof coutry);
+console.log(typeof language);
+ */
