74436cf9f1016a1c14d8edb08d56dce6d1d99f11
aurrelkondi <aurrelkondi@hotmail.com>
2023-03-02 14:57:01 +0100

    The for of loop

--- index.js
+++ index.js
@@ -751,7 +751,7 @@ const restaurant = {
 	},
 };
 
-//Destructuring OBJECTS, the way how to do it...
+/* //Destructuring OBJECTS, the way how to do it...
 const { name, categories, openingHours } = restaurant;
 // console.log(name, openingHours, categories);
 
@@ -773,6 +773,9 @@ console.log(mainMenuCopy);
 const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
 console.log(menu);
 
+const newRestaurant = { ...restaurant, founder: "Aurrel" };
+console.log(newRestaurant);
+
 //Destructuring ARRAYS, the way how to do it...
 let [main, secondary] = restaurant.categories;
 [main, secondary] = [secondary, main];
@@ -784,3 +787,120 @@ const [starter, mainCourse] = restaurant.order(2, 0);
 const number = [1, 2, 3];
 const [a, b, c] = number;
 // console.log(a, b, c);
+
+//Rest Pattern
+
+const [pizza, risotto, ...otherFood] = [
+	...restaurant.mainMenu,
+	...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+console.log(3 || "Joneas");
+console.log(3 && "jonas" && "hi");
+ */
+
+//Data Structures, Modern Operators and Strings
+//Coding Challenge #1
+/* We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game ('game' variable on next page). In this challenge we're gonna work with that data.
+Your tasks:
+1. Createoneplayerarrayforeachteam(variables'players1'and 'players2')
+2. Thefirstplayerinanyplayerarrayisthegoalkeeperandtheothersarefield players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Createanarray'allPlayers'containingallplayersofbothteams(22 players)
+4. Duringthegame,BayernMunich(team1)used3substituteplayers.Socreatea new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Basedonthegame.oddsobject,createonevariableforeachodd(called 'team1', 'draw' and 'team2')
+6. Writeafunction('printGoals')thatreceivesanarbitrarynumberofplayer names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. Theteamwiththeloweroddismorelikelytowin.Printtotheconsolewhich team is more likely to win, without using an if/else statement or the ternary operator.
+Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+ */
+/* const game = {
+	team1: "Bayern Munich",
+	team2: "Borrussia Dortmund",
+	players: [
+		[
+			"Neuer",
+			"Pavard",
+			"Martinez",
+			"Alaba",
+			"Davies",
+			"Kimmich",
+			"Goretzka",
+			"Coman",
+			"Muller",
+			"Gnarby",
+			"Lewandowski",
+		],
+		[
+			"Burki",
+			"Schulz",
+			"Hummels",
+			"Akanji",
+			"Hakimi",
+			"Weigl",
+			"Witsel",
+			"Hazard",
+			"Brandt",
+			"Sancho",
+			"Gotze",
+		],
+	],
+	score: "4:0",
+	scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+	date: "Nov 9th, 2037",
+	odds: {
+		team1: 1.33,
+		x: 3.25,
+		team2: 6.5,
+	},
+};
+
+//1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+//2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers); */
+/* 
+//3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4.
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+
+//5.
+const {
+	odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+//6.
+const printGoals = function (...players) {
+	console.log(`${players.length} goals were scored`);
+};
+
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals(...game.scored);
+
+//7.
+
+team1 < team2 && console.log("Team 1 is more likely to win"); */
+
+//The for-of Loop
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+for (const item of menu.entries()) console.log(item);
+
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+	console.log(day);
+}
